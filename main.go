180d3abf1c5aa557ff2568062f47f035a91f6039
Benoit Pimpaud <pimpaudben@gmail.com>
2024-02-04 17:46:19 +0100

    [conf] init API

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"rel/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
